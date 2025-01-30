05eea70ebcb0ee3c61f259bc96c488c9f7268f6a
Your Name <you@example.com>
2025-01-30 18:23:06 +0100

    add magit

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -57,6 +57,9 @@
   :custom
   (indent-tabs-mode nil))
 
+(use-package magit
+  :ensure t)
+
 (use-package org
   :custom
   (org-agenda-files nil)
@@ -96,3 +99,15 @@
     (backward-char 1)
     (insert "-")
     (org-insert-time-stamp (current-time) t)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(magit)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
