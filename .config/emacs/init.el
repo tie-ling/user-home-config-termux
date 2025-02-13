164c15ca8193c3c8da83ec71a8032cf927d95359
nix-on-droid <you@example.com>
2025-02-13 09:46:40 +0000

    emacs set default coding system to utf8

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,3 +1,5 @@
+(set-default-coding-systems 'utf-8)
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
@@ -140,16 +142,4 @@
 
 (use-package haskell-ts-mode
   :ensure t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(haskell-ts-mode ledger-mode counsel magit nix-mode pyim-basedict pyim)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
