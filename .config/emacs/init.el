590feadaee9dbd6a7f64f1ad91a3b76d94bf53d7
nix-on-droid <you@example.com>
2025-02-17 13:32:05 +0100

    add termux color
    Signed-off-by: nix-on-droid <you@example.com>

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -88,3 +88,15 @@
      ("s" . "src")
      ("v" . "verse")
      ("py" . "src python"))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(magit ledger-mode counsel)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
