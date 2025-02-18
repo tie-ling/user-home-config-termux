c77fea0d4c10f4423efe71d1d1343c956334331a
nix-on-droid <you@example.com>
2025-02-18 10:14:01 +0100

    add ledger
    Signed-off-by: nix-on-droid <you@example.com>

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,3 +1,7 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
 (use-package emacs
   :custom
   (xterm-mouse-mode nil)
@@ -76,15 +80,10 @@
      ("s" . "src")
      ("v" . "verse")
      ("py" . "src python"))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit ledger-mode counsel)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(use-package ledger-mode
+  :ensure t
+  :custom
+  ((ledger-report-use-strict t)
+   (ledger-default-date-format "%y-%m-%d"))
+  :mode ("\\.ledger\\'"))
