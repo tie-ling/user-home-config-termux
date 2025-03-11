1820565cce68989a1cd26425bf31490d360f9779
nix-on-droid <you@example.com>
2025-03-11 09:35:48 +0100

    rm pyim

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -8,7 +8,6 @@
   (auto-fill-function 'do-auto-fill t)
   (calendar-week-start-day 1)
   (custom-enabled-themes '(modus-vivendi) nil nil)
-  (default-input-method "pyim")
   (display-battery-mode nil)
   (display-time-mode nil)
   (electric-pair-mode t)
@@ -85,21 +84,12 @@
    (ledger-default-date-format "%y-%m-%d"))
   :mode ("\\.ledger\\'"))
 
-(use-package pyim
-  :ensure t)
-
-(use-package pyim-basedict
-  :ensure t
-  :requires (pyim)
-  :config
-  (pyim-basedict-enable))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(ledger-mode magit counsel)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
